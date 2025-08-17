aa8f4e7ae94be05cfe765526c53143e04a4a0e67
Spy230 <121127523+Spy230@users.noreply.github.com>
2025-08-17 12:53:27 +0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -12,7 +12,7 @@ const Index = () => {
       <AnimatedBackground />
       <Header />
 
-      <main className="pt-[140px] sm:pt-[120px]">
+      <main>
         <HeroSection />
         <CasesSection />
         <ContactForm />
