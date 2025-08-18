4db2d49fa9d3832e5e8ef339d3193d20df3f58f1
Spy230 <121127523+Spy230@users.noreply.github.com>
2025-08-18 22:36:27 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import IndexSimple from "./pages/IndexSimple";
+import IndexDebug from "./pages/IndexDebug";
+import ThankYou from "./pages/ThankYou";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -16,6 +19,9 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/debug" element={<IndexDebug />} />
+          <Route path="/simple" element={<IndexSimple />} />
+          <Route path="/thank-you" element={<ThankYou />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
