102c2b840b66433fa83350719277204801b87b05
Spy230 <121127523+Spy230@users.noreply.github.com>
2025-09-12 22:46:28 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,11 @@ import IndexSimple from "./pages/IndexSimple";
 import IndexDebug from "./pages/IndexDebug";
 import ThankYou from "./pages/ThankYou";
 import NotFound from "./pages/NotFound";
+import PrivacyPolicy from "./pages/legal/PrivacyPolicy";
+import TermsOfService from "./pages/legal/TermsOfService";
+import DataProcessingConsent from "./pages/legal/DataProcessingConsent";
+import MarketingConsent from "./pages/legal/MarketingConsent";
+import LegalDemo from "./pages/LegalDemo";
 
 const queryClient = new QueryClient();
 
@@ -22,6 +27,14 @@ const App = () => (
           <Route path="/debug" element={<IndexDebug />} />
           <Route path="/simple" element={<IndexSimple />} />
           <Route path="/thank-you" element={<ThankYou />} />
+          <Route path="/legal-demo" element={<LegalDemo />} />
+          
+          {/* Legal pages */}
+          <Route path="/legal/privacy" element={<PrivacyPolicy />} />
+          <Route path="/legal/terms" element={<TermsOfService />} />
+          <Route path="/legal/data-processing" element={<DataProcessingConsent />} />
+          <Route path="/legal/marketing" element={<MarketingConsent />} />
+          
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
