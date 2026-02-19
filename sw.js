6f40761f01a76d1a7b1323da63f26d61b2474eff
Spy <shesterikovon@gmail.com>
2026-02-19 17:42:25 +0300

    Updates

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'phenix-project-v1';
+const urlsToCache = [
+  '/',
+  '/src/main.tsx',
+  '/src/index.css',
+  'https://a.imgfoto.host/2025/07/30/GLAVNOE-FOTO.png'
+];
+
+// Установка Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Обработка запросов
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Возвращаем кэшированную версию или загружаем из сети
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Обновление кэша
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
